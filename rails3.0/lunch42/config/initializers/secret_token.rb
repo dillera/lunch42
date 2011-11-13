02a16167c4edb9d54d4e5108c653af7adc28dd0c
andy diller <dillera@dillernet.com>
2011-11-13 13:58:27 -0500

    rails 3.0.8

---
+++ rails3.0/lunch42/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lunch42::Application.config.secret_token = '2185dfdaa8cc848c77c4777c478c0234166ad51657b164bc2404b4a9002f142f135d05f2f54785e49529bbb17764f0263b4144c56a0d7d16222dd66f165c79ef'
