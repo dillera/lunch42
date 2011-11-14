1ad39381bd7c930cba71597541a56544420aa0f5
andy diller <dillera@dillernet.com>
2011-11-13 22:02:31 -0500

    ran cuke and rpsec gen

--- rails3.0/lunch42/config/application.rb
+++ rails3.0/lunch42/config/application.rb
@@ -38,5 +38,13 @@ module Lunch42
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
+
+    config.generators do |g|
+    g.test_framework :rspec
+    end
+
   end
+
 end
+
+
