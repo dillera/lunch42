72c2d58ca8ac344defc45f8717683ac46d1ed74d
Andrew Diller <dillera@dillernet.com>
2011-11-20 23:43:37 -0500

    removed old rails app

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Lunch42::Application.config.secret_token = '1c9aef8d769bba460f9c08619b354edde4f4d34176e5da1d809c919c5fa114dff3360249e962e4853bbe1f3b9e580c07f1dead03ab2a10cf97708d17bffbcbda'
